7829076bab1e11ff3612685c49bedf96f3a3ca39
Adrien <adrien.bordeaux33@outlook.com>
2025-10-12 11:01:53 +0200

    ajout des moves avec possibilités d ajouter / supprimer les moves de la liste

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,14 +2,33 @@ type CardProps = {
   name: string;
   color: string;
   difficulty: string;
+  addOneMove: (newMove: string) => void;
+  removeAllMove: (moveToRemove: string) => void;
 };
 
 export default function Card(props: CardProps) {
   return (
-    <div className=" flex flex-col m-4 p-4 border-4 border-black rounded-lg bg-gray-800">
+    <div className=" flex flex-col items-center m-4 p-4 border-4 border-black rounded-lg bg-gray-800">
       <div>name : {props.name}</div>
       <div>color : {props.color}</div>
       <div>difficulty : {props.difficulty}</div>
+      <div>
+        <button
+          className="m-2 btn-swb-main text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            props.addOneMove(props.name);
+            console.log("selected move : ", props.name);
+          }}
+        >
+          Select
+        </button>
+        <button
+          className="m-2 btn-swb-main text-white font-bold py-2 px-4 rounded"
+          onClick={() => props.removeAllMove(props.name)}
+        >
+          UnSelect
+        </button>
+      </div>
     </div>
   );
 }
--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,6 +1,6 @@
 export default function Menu() {
   return (
-    <div className="flex flex-col text-white border-4 border-yellow-500 p-4 justify-center ">
+    <div className="flex flex-col text-white border-4 border-gray-500 p-4 justify-center ">
       <div className="flex flex gap-8 items-center text-2xl">Menu</div>
       <div className=" flex flex-col gap-6  text-xl pt-8">
         <div>Médias</div>
